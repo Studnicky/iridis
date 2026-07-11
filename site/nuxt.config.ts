bbfb554b2fbf3a9afa4b09a7aa241dbc5ed3486b
Andrew Studnicky <a.j.studnicky@gmail.com>
2026-07-10 22:04:34 -0400

    config: disable SSR to make Iridis a proper client-side SPA

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  ssr: false, // Iridis is a client-side interactive app; disable SSR for GitHub Pages SPA
   modules: [
     '@nuxt/ui',
     '@nuxt/content',
